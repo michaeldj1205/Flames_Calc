410040d1334aa002eb28007214e248d5e5113c99
michaeldj1205 <149767744+michaeldj1205@users.noreply.github.com>
2025-02-08 21:31:56 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function calculateFLAMES() {
+    let name1 = document.getElementById("name1").value.toLowerCase().replace(/\s/g, '');
+    let name2 = document.getElementById("name2").value.toLowerCase().replace(/\s/g, '');
+
+    if (!name1 || !name2) {
+        document.getElementById("result").innerText = "⚠ Please enter both names!";
+        document.getElementById("result-container").style.opacity = "1";
+        return;
+    }
+
+    let name1Arr = name1.split("");
+    let name2Arr = name2.split("");
+
+    name1Arr.forEach(letter => {
+        let index = name2Arr.indexOf(letter);
+        if (index !== -1) {
+            name2Arr.splice(index, 1);
+            name1Arr.splice(name1Arr.indexOf(letter), 1);
+        }
+    });
+
+    let remainingCount = name1Arr.length + name2Arr.length;
+    let flames = ["Friends", "Lovers", "Affection", "Marriage", "Enemies", "Siblings"];
+
+    let index = (remainingCount % flames.length) - 1;
+    if (index < 0) {
+        index = flames.length - 1;
+    }
+
+    document.getElementById("result").innerText = `💖 Your relationship is: ${flames[index]}! 💖`;
+    document.getElementById("result-container").style.opacity = "1";
+}
